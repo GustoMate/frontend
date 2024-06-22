c3dd2afab16f6792f4b1989811d0a7993fd2b76c
hengyinayo <wuheng@yonsei.ac.kr>
2024-06-22 15:32:42 +0900

    feat: 회원가입 및 설문조사 1차 structure

--- GustoMateApp/src/App.tsx
+++ GustoMateApp/src/App.tsx
@@ -1,64 +1,20 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignIn from './screens/SignIn';
+import Survey from './screens/Survey';
 
-export type Props = {
-  name: string;
-  baseEnthusiasmLevel?: number;
-}
+const Stack = createNativeStackNavigator();
 
-const App: React.FC<Props> = ({
-  name,
-  baseEnthusiasmLevel = 0,
-}) => {
-  const [enthusiasmLevel, setEnthusiasmLevel] = React.useState(
-    baseEnthusiasmLevel
-  );
-
-  const onIncrement = () => 
-    setEnthusiasmLevel(enthusiasmLevel + 1);
-  const onDecrement = () =>
-    setEnthusiasmLevel(
-      enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0,
-    );
-
-  const getExclamationMarks = (numChars: number) => 
-    numChars > 0 ? Array(numChars + 1).join('!') : '';
-
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.greeting}>
-        Hello {name}
-        {getExclamationMarks(enthusiasmLevel)}
-      </Text>
-      <View>
-        <Button
-          title="Increase enthusiasm"
-          accessibilityLabel='increment'
-          onPress={onIncrement}
-          color="blue"
-        />
-        <Button
-          title="Decrease enthusiasm"
-          accessibilityLabel="decrement"
-          onPress={onDecrement}
-          color="red"
-        />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false}}>
+        {/*<Stack.Screen name="SignIn" component={SignIn} /> */}
+        <Stack.Screen name="Survey" component={Survey} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16,
-  },
-});
 
 export default App;
---
+++ GustoMateApp/src/constants/images.tsx
@@ -0,0 +1,7 @@
+import SignInBackground from '../assets/images/signIn.jpg';
+import GustomateLogo from '../assets/images/gustomate_logo.png';
+
+export default {
+    SignInBackground,
+    GustomateLogo,
+};
